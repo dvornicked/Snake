0784df5c157e92b8802548c41e98803bcc172031
Dvornick <mail@dvornick.ru>
2021-10-17 17:17:03 +0300

    Preloader for sprites

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
  let game =  {
+    canvas: null,
+     ctx: null,
+     sprites: {
+         background: null,
+         cell: null,
+     },
     start() {
-        let canvas = document.getElementById('myCanvas')
-        let ctx = canvas.getContext('2d')
+        this.canvas = document.getElementById('myCanvas')
+        this.ctx = this.canvas.getContext('2d')
 
-        let background = new Image()
-        background.src = './img/background.png'
-        background.addEventListener('load', () => {
-            window.requestAnimationFrame(() => {
-                ctx.drawImage(background, 0, 0)
-            })
+        this.preload(() => {
+            this.run()
         })
-    }
+    },
+     preload(callback) {
+        let loaded = 0
+        let required = Object.keys(this.sprites).length
+        let onAssetLoad = () => {
+            ++loaded
+
+            if (loaded >= required) {
+                callback()
+            }
+        }
+         for (let key in this.sprites) {
+             this.sprites[key] = new Image()
+             this.sprites[key].src = `./img/${key}.png`
+             this.sprites[key].addEventListener('load', onAssetLoad)
+         }
+         
+         callback()
+     },
+     run() {
+         window.requestAnimationFrame(() => {
+             this.ctx.drawImage(this.sprites.background, 0, 0)
+             this.ctx.drawImage(this.sprites.cell, 320, 180)
+         })
+     }
 }
 
 game.start()
