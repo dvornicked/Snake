33f985a021d219d51aa46da2c8069154f222311a
Dvornick <mail@dvornick.ru>
2021-10-17 19:30:49 +0300

    Canvas size control

--- src/index.js
+++ src/index.js
@@ -6,11 +6,55 @@ let game =  {
          cell: null,
          body: null
      },
-    width: 640,
-    height: 360,
+    width: 0,
+    height: 0,
+    dimensions: {
+        max: {
+            width: 640,
+            height: 360
+        },
+        min: {
+            width: 300,
+            height: 300
+        }
+    },
     init() {
         this.canvas = document.getElementById('myCanvas')
         this.ctx = this.canvas.getContext('2d')
+        this.initDimensions()
+    },
+    initDimensions() {
+        let data = {
+            maxWidth: this.dimensions.max.width,
+            maxHeight: this.dimensions.max.height,
+            minWidth: this.dimensions.min.width,
+            minHeight: this.dimensions.min.height,
+            realWidth: window.innerWidth,
+            realHeight: window.innerHeight
+        }
+
+        if (data.realWidth / data.realHeight > data.maxWidth / data.maxHeight) {
+            this.fitWidth(data)
+        } else {
+            this.fitHeight(data)
+        }
+
+        this.canvas.width = this.width
+        this.canvas.height = this.height
+    },
+    fitHeight(data) {
+        this.width = Math.round(data.realWidth * data.maxHeight / data.realHeight)
+        this.width = Math.min(this.width, data.maxWidth)
+        this.width = Math.max(this.width, data.minWidth)
+        this.height = Math.round(this.width * data.realHeight / data.realWidth)
+        this.canvas.style.height = '100%'
+    },
+    fitWidth(data) {
+        this.height = Math.round(this.width * data.realHeight / data.realWidth)
+        this.height = Math.min(this.height, data.maxHeight)
+        this.height = Math.max(this.height, data.minHeight)
+        this.width = Math.round(data.realWidth * this.height / data.realHeight)
+        this.canvas.style.width = '100%'
     },
     start() {
         this.init()
@@ -38,7 +82,7 @@ let game =  {
         this.board.create()
          this.snake.create()
          window.requestAnimationFrame(() => {
-             this.ctx.drawImage(this.sprites.background, 0, 0)
+             this.ctx.drawImage(this.sprites.background, (this.width - this.sprites.background.width) / 2, (this.height - this.sprites.background.height) / 2)
              this.board.render()
              this.snake.render()
          })
