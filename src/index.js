522d17b62dab9b1ccc74b69b5c4d6784e677730c
Dvornick <mail@dvornick.ru>
2021-10-18 17:57:46 +0300

    Drawing the head of a snake

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ let game =  {
          background: null,
          cell: null,
          body: null,
-         food: null
+         food: null,
+         head: null
      },
     width: 0,
     height: 0,
@@ -198,10 +199,18 @@ game.snake = {
             this.cells.push(cell)
         }
     },
+    renderHead() {
+        let head = this.cells[0]
+        this.game.ctx.drawImage(this.game.sprites.head, head.x, head.y)
+    },
+    renderBody() {
+        for (let i = 1; i < this.cells.length; i++) {
+            this.game.ctx.drawImage(this.game.sprites.body, this.cells[i].x, this.cells[i].y)
+        }
+    },
     render() {
-        this.cells.forEach(cell => {
-            this.game.ctx.drawImage(this.game.sprites.body, cell.x, cell.y)
-        })
+        this.renderHead()
+        this.renderBody()
     },
     start(key) {
         switch (key) {
